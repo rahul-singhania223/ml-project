0b9645b862095af7aca8d7b50ed3e7ab5f5b391a
rahul-singhania223 <rahulsinghania406@gmail.com>
2025-03-23 11:13:30 +0530

    solved all bugs and now model is working good

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,10 @@ from sklearn.model_selection import train_test_split
 
 from src.exception import CustomException
 from src.logger import logging
+
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
+from src.components.model_training import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -44,8 +47,8 @@ class DataIngestion:
             )
             
         except Exception as e:
-            # raise CustomException(e, sys)
-            pass
+            raise CustomException(e, sys)
+            
         
 
 if __name__ == "__main__":
@@ -53,4 +56,10 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+
+    print("Score: ", score)
+
